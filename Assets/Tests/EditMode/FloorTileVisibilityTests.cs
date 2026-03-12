d68b034e772c6aa611bd93f51a5114e99d5480c9
Miles Lee <miles.lee@buzzvil.com>
2026-03-13 01:45:31 +0900

    fix: floor tile visible

---
+++ Assets/Tests/EditMode/FloorTileVisibilityTests.cs
@@ -0,0 +1,100 @@
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Ubongo.Tests.EditMode
+{
+    public class FloorTileVisibilityTests
+    {
+        private GameObject tileRoot;
+        private FloorTileView tile;
+        private Renderer cellRenderer;
+        private GameObject gridOverlay;
+        private BoxCollider tileCollider;
+
+        [SetUp]
+        public void SetUp()
+        {
+            tileRoot = new GameObject("CellRoot");
+
+            tileCollider = tileRoot.AddComponent<BoxCollider>();
+            tileCollider.isTrigger = true;
+
+            GameObject visual = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            visual.name = "Visual";
+            visual.transform.SetParent(tileRoot.transform, false);
+            cellRenderer = visual.GetComponent<Renderer>();
+
+            gridOverlay = new GameObject("GridOverlay");
+            gridOverlay.transform.SetParent(tileRoot.transform, false);
+
+            tile = tileRoot.AddComponent<FloorTileView>();
+            tile.Initialize(0, 0);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(tileRoot);
+        }
+
+        [Test]
+        public void Apply_NonTargetNonOccupied_DisablesRenderer()
+        {
+            var state = new FloorTileVisualState(false, false, FloorTileHighlightMode.None);
+
+            tile.Apply(state);
+
+            Assert.IsFalse(cellRenderer.enabled);
+        }
+
+        [Test]
+        public void Apply_TargetNonOccupied_EnablesRenderer()
+        {
+            var state = new FloorTileVisualState(true, false, FloorTileHighlightMode.None);
+
+            tile.Apply(state);
+
+            Assert.IsTrue(cellRenderer.enabled);
+        }
+
+        [Test]
+        public void Apply_NonTargetThenTarget_ReEnablesRenderer()
+        {
+            tile.Apply(new FloorTileVisualState(false, false, FloorTileHighlightMode.None));
+            Assert.IsFalse(cellRenderer.enabled);
+
+            tile.Apply(new FloorTileVisualState(true, false, FloorTileHighlightMode.None));
+            Assert.IsTrue(cellRenderer.enabled);
+        }
+
+        [Test]
+        public void Apply_NonTarget_DisablesGridOverlay()
+        {
+            var state = new FloorTileVisualState(false, false, FloorTileHighlightMode.None);
+
+            tile.Apply(state);
+
+            Assert.IsFalse(gridOverlay.activeSelf);
+        }
+
+        [Test]
+        public void Apply_Target_EnablesGridOverlay()
+        {
+            // First disable, then re-enable via target state
+            tile.Apply(new FloorTileVisualState(false, false, FloorTileHighlightMode.None));
+            tile.Apply(new FloorTileVisualState(true, false, FloorTileHighlightMode.None));
+
+            Assert.IsTrue(gridOverlay.activeSelf);
+        }
+
+        [Test]
+        public void Apply_NonTarget_DisablesCollider()
+        {
+            var state = new FloorTileVisualState(false, false, FloorTileHighlightMode.None);
+
+            tile.Apply(state);
+
+            Assert.IsFalse(tileCollider.enabled);
+        }
+    }
+}
